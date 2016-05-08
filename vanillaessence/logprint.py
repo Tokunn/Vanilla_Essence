416e55540f929820e758b7cc90fd7e88f25cd70e
Your Name <you@example.com>
2016-05-08 19:19:50 +0900

    fix logprint

--- vanillaessence/logprint.py
+++ vanillaessence/logprint.py
@@ -12,7 +12,7 @@ STARTUP_TIME = time.time()
 
 def uptime():
     """ Return STR uptime """
-    return str(time.time() - STARTUP_TIME)[:8]
+    return str(time.time() - STARTUP_TIME)[:10]
 
 def getstartup_time():
     """ Return FLOAT Startup time """
@@ -20,20 +20,20 @@ def getstartup_time():
 
 def logdebug(strings):
     """ Print Debug """
-    log = "\033[32m[{}] [DEBUG] {}\033[0m".format(uptime(), strings)
+    log = "\033[32m[DEBUG] [{}]: {}\033[0m".format(uptime(), strings)
     print(log)
 
 def loginfo(strings):
     """ Print Info """
-    log = "\033[01m[{}] [INFO]  {}\033[0m".format(uptime(), strings)
+    log = "\033[01m[ INFO] [{}]: {}\033[0m".format(uptime(), strings)
     print(log)
 
 def logwarn(strings):
     """ Print Warn """
-    log = "\033[93m[{}] [WARN]  {}\033[0m".format(uptime(), strings)
+    log = "\033[93m[ WARN] [{}]: {}\033[0m".format(uptime(), strings)
     print(log)
 
 def logerr(strings):
     """ Print Err """
-    log = "\033[91m[{}] [ERR]   {}\033[0m".format(uptime(), strings)
+    log = "\033[91m[  ERR] [{}]: {}\033[0m".format(uptime(), strings)
     print(log)
